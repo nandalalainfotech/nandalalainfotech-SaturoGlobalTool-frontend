1142f5c9d71fe9acfd0182ac985ddac9d4dc1a00
nandalalainfotech@gmail.com <Spartans!23>
2022-07-07 16:50:22 +0530

    Task Allocation and task status component creation done by sekar.

--- src/app/dash-board/admin/admin-routing.module.ts
+++ src/app/dash-board/admin/admin-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { TaskAllocationComponent } from './task-allocation/task-allocation.component';
+import { TaskStatusComponent } from './task-status/task-status.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: AdminComponent
+    component: AdminComponent,
+
+    children: [
+      {
+        path: 'app-task-allocation',
+        component: TaskAllocationComponent,
+      },
+      {
+        path: 'app-task-status',
+        component: TaskStatusComponent,
+      }
+    ]
   }
 ];
 
