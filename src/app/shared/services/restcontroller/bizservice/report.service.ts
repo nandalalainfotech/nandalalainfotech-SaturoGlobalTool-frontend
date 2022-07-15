04b12b0efed9a1609be264353c696e1870d32b90
Nandalalainfotech <nandalalainfotech.com>
2022-07-15 13:54:38 +0530

    FrondEnd reviewer export excel done by Moorthy.

--- src/app/shared/services/restcontroller/bizservice/report.service.ts
+++ src/app/shared/services/restcontroller/bizservice/report.service.ts
@@ -20,4 +20,19 @@ export class LigandReportsManager extends BaseService {
         return this.getCallService1(`${this.ligandeReportsUrl}` + "/Tanexcel", data)
     }
 
+    batchNumberExportExcel(username: any,rbatchNo: any) {    
+        let data: any = {};
+        data['username'] = username;
+        data['cbatchNo'] = rbatchNo;
+        return this.getCallService1(`${this.ligandeReportsUrl}` + "/batchNumberExportExcel", data)
+    }
+
+    startEndDateExportExcel(username: any,startDate: any,endDate: any) {    
+        let data: any = {};
+        data['username'] = username;
+        data['startDate'] = startDate;
+        data['endDate'] = endDate;
+        return this.getCallService1(`${this.ligandeReportsUrl}` + "/startEndDateExportExcel", data)
+    }
+
 }
